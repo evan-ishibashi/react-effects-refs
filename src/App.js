901bc2a6c8782a8c83787ad31833b8ba0743ae14
Gabe Yoro <gabrielyoro@gmail.com>
2023-10-03 12:40:38 -0700

    Final solution pre-code review

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
-import  DrawCard  from "./DrawCard"
+import DrawCard from "./DrawCard";
 const BASE_URL = "https://deckofcardsapi.com/api/deck/";
 
+/**
+ * Main App
+ * State:
+ * - deck : current deck data {isLoading, data, currCard, errors}
+ *
+ * App -> DrawCard
+ */
 function App() {
-    // state {
-    //     "success": true,
-    //     "deck_id": "3p40paa87x90",
-    //     "shuffled": false,
-    //     "remaining": 52
-    // }
     const [deck, setDeck] = useState({
         isLoading: true,
         data: null,
@@ -27,13 +28,13 @@ function App() {
                     isLoading: false,
                     data,
                     currCard: null,
-                    errors: null
+                    errors: null,
                 });
             } catch (err) {
                 setDeck({
                     isLoading: false,
-                    data:null,
-                    currCard:null,
+                    data: null,
+                    currCard: null,
                     errors: err,
                 });
             }
@@ -41,26 +42,29 @@ function App() {
         getDeck();
     }, []);
 
-    async function drawOne(){
-        const resp = await fetch(`${BASE_URL}${deck.data.deck_id}/draw/?count=1`);
+    /** Fetch one card from API */
+    async function drawOne() {
+        const resp = await fetch(
+            `${BASE_URL}${deck.data.deck_id}/draw/?count=1`
+        );
         const cardData = await resp.json();
-        console.log("cardData",cardData)
+        console.log("cardData", cardData);
         setDeck({
-                    data : {...deck.data, remaining:(cardData.remaining)},
-                    isLoading: false,
-                    currCard: cardData.cards[0],
-                    errors: null,
+            data: { ...deck.data, remaining: cardData.remaining },
+            isLoading: false,
+            currCard: cardData.cards[0],
+            errors: null,
         });
     }
-
-
 
     if (deck.isLoading) return <h2>LOADING...</h2>;
     else if (deck.errors) return <b>Oh no! {deck.errors}</b>;
 
-    return <div className="App">
-        <DrawCard deck={deck} drawOne={drawOne}/>
-    </div>;
+    return (
+        <div className="App">
+            <DrawCard deck={deck} drawOne={drawOne} />
+        </div>
+    );
 }
 
 export default App;
--- src/DrawCard.js
+++ src/DrawCard.js
@@ -1,18 +1,26 @@
+/**
+ * DrawCard
+ * Prop:
+ * - deck : deck data
+ * - drawOne : handles click on button to fetch card
+ *
+ * App -> DrawCard
+ */
+function DrawCard({ deck, drawOne }) {
+    async function handleClick() {
+        const card = await drawOne();
+    }
 
-
-function DrawCard({ deck, drawOne}) {
-
-  async function handleClick(){
-    const card = await drawOne();
-  }
-
-  return(
-    <>
-    <button onClick={handleClick}>DrawCard</button><br/>
-    {(deck.currCard) && <img src={`${deck.currCard.image}`}/>}
-
-    </>
-  );
+    return (
+        <>
+            <button onClick={handleClick}>DrawCard</button>
+            <br />
+            {deck.data.remaining === 0 && <p>Error: no cards remaining!</p>}
+            {deck.data.remaining < 52 && deck.data.remaining > 0 && (
+                <img src={`${deck.currCard.image}`} />
+            )}
+        </>
+    );
 }
 
 export default DrawCard;
