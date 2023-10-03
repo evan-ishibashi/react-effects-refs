ef852f911797ee438b1071388174b4702c279e25
Gabe Yoro <gabrielyoro@gmail.com>
2023-10-03 11:32:49 -0700

    Initialized useEffect and state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+const BASE_URL = "https://deckofcardsapi.com/api/deck/";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // state {
+    //     "success": true,
+    //     "deck_id": "3p40paa87x90",
+    //     "shuffled": false,
+    //     "remaining": 52
+    // }
+    const [deck, setDeck] = useState({
+        isLoading: true,
+        data: null,
+        error: null,
+    });
+
+    useEffect(() => {
+        async function getDeck() {
+            try {
+                const response = await fetch(`${BASE_URL}new/`);
+                const data = await response.json();
+                setDeck({ data, isLoading: false, errors: null });
+            } catch (err) {
+                setDeck({
+                    data,
+                    isLoading: false,
+                    errors: err,
+                });
+            }
+        }
+        getDeck();
+    }, []);
+
+    if (deck.isLoading) return <h2>LOADING...</h2>;
+    else if (deck.error) return <b>Oh no! {error}</b>;
+
+    return <div className="App"></div>;
 }
 
 export default App;
